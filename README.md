e244a4424d3b1b82ed9756fc3723c6e69a16befe
Annka-Sarviro <annka_nuha@i.ua>
2025-01-07 15:53:49 +0200

    create

---
+++ README.md
@@ -0,0 +1,57 @@
+# ⚡ VITE Starter kit
+
+занеобхідністю встановити nodemon
+
+```
+npm install -g nodemon - глобально
+
+```
+
+npm install --save-dev nodemon - локально для цього проєкту
+
+## 1) Запуск збірки
+
+Встановити залежності проєкту:
+
+```
+pnpm install / yarn / npm i
+```
+
+Запустити проєкт в режимі розробки:
+
+```
+pnpm dev / yarn dev / npm dev
+```
+
+Створити локальний білд:
+
+```
+pnpm preview / yarn preview / npm run preview
+```
+
+Створити білд для продакшену:
+
+```
+pnpm build / yarn build / npm run build
+```
+
+## 2) Інформація про збірку
+
+- В збірці використовується шаблонизатор
+  [Nunjucks](https://mozilla.github.io/nunjucks/templating.html)
+
+  > Сторінки проекту зберігаються у папці **«src/assets/html/pages»**.
+  >
+  > Секційні фрагменти зберігаються у папці **«src/assets/html/sections**.
+
+- В збірці використовується CSS framework [Tailwind](https://tailwindcss.com/docs/installation) та
+  CSS з nesting.
+
+- В збірці є автоматична оптимізація усіх зображень.
+
+- В збірці є автоматичне створення WEBP зображень. Покладіть jpg або png зображення і збірка
+  автоматично створить webp.
+
+- В збірці автоматично збираєтсья svg-sprite. Для цього помістити svg файл в папку
+  assets/images/sprite. Для звертання до нього використати
+  <use xlink:href="/spritemap.svg#svg-name"></use>,
---
+++ vite.config.js
@@ -0,0 +1,66 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import nunjucks from 'vite-plugin-nunjucks';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import data from './src/assets/json/data.json';
+import { svgSpritemap } from 'vite-plugin-svg-spritemap';
+
+// other config settings: https://vitejs.dev/config/
+export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  base: './',
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    open: true,
+    hmr: true,
+  },
+  build: {
+    outDir: '../build',
+    emptyOutDir: true,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/js/app.min.js',
+        chunkFileNames: 'assets/js/[name].js',
+        assetFileNames: assetInfo => {
+          const fileName = assetInfo.name;
+          const extName = path.extname(fileName);
+
+          switch (true) {
+            case fileName === 'style.css':
+              return 'assets/css/style.css';
+
+            case extName === '.jpg' ||
+              extName === '.jpeg' ||
+              extName === '.png' ||
+              extName === '.webp' ||
+              extName === '.svg' ||
+              extName === '.gif':
+              return `assets/images/[name][extname]`;
+
+            default:
+              return 'assets/[name][extname]';
+          }
+        },
+      },
+    },
+  },
+  plugins: [
+    nunjucks({
+      variables: { 'index.html': data },
+      nunjucksConfigure: { autoescape: false },
+    }),
+    createHtmlPlugin({
+      // https://github.com/vbenjs/vite-plugin-html/tree/main?tab=readme-ov-file#minifyoptions
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true,
+      },
+    }),
+    svgSpritemap({
+      pattern: 'src/assets/images/sprite/*.svg',
+    }),
+  ],
+});
